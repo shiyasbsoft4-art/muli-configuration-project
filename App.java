fa6f2bc99623f46e5bfcdbc27ec1c609fcd153ff
shiyasbsoft4-art <shiyasbsoft4@gmail.com>
2026-01-09 10:19:30 +0530

    Create App.java with main method and output messages

---
+++ App.java
@@ -0,0 +1,9 @@
+package com.example.app;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, this is a sample Java Maven application.");
+        System.out.println("Application executed successfully.");
+    }
+}
